213f2dacdd59d8ee1884ecdd39aa7727cef07f61
Petre Leonard Macamete <petreleonardos@gmail.com>
2020-02-07 20:32:15 +0200

    pizza

--- main.py
+++ main.py
@@ -1,58 +1,25 @@
-# Python3 code for Dynamic Programming
-# based solution for 0-1 Knapsack problem
+from __future__ import print_function
+from ortools.algorithms import pywrapknapsack_solver
 
-# Prints the items which are put in a
-# knapsack of capacity W
-def printknapSack(W, wt, val, n):
-    K = [[0 for w in range(W + 1)]
-         for i in range(n + 1)]
+def main():
+      # Create the solver.
+      solver = pywrapknapsack_solver.KnapsackSolver(
+      pywrapknapsack_solver.KnapsackSolver.
+      KNAPSACK_DYNAMIC_PROGRAMMING_SOLVER,
+      'test')
 
-    # Build table K[][] in bottom
-    # up manner
-    for i in range(n + 1):
-        for w in range(W + 1):
-            if i == 0 or w == 0:
-                K[i][w] = 0
-            elif wt[i - 1] <= w:
-                K[i][w] = max(val[i - 1]
-                              + K[i - 1][w - wt[i - 1]],
-                              K[i - 1][w])
-            else:
-                K[i][w] = K[i - 1][w]
+      weights = [[2, 5, 6, 8]]
+      capacities = [17]
+      values = weights[0]
+      solver.Init(values, weights, capacities)
+      computed_value = solver.Solve()
 
-            # stores the result of Knapsack
-    res = K[n][W]
-    print(res)
+      packed_items = [x for x in range(0, len(weights[0]))
+                if solver.BestSolutionContains(x)]
+      packed_weights = [weights[0][i] for i in packed_items]
 
-    w = W
-    for i in range(n, 0, -1):
-        if res <= 0:
-            break
-        # either the result comes from the
-        # top (K[i-1][w]) or from (val[i-1]
-        # + K[i-1] [w-wt[i-1]]) as in Knapsack
-        # table. If it comes from the latter
-        # one/ it means the item is included.
-        if res == K[i - 1][w]:
-            continue
-        else:
-
-            # This item is included.
-            print(wt[i - 1])
-
-            # Since this weight is included
-            # its value is deducted
-            res = res - val[i - 1]
-            w = w - wt[i - 1]
-
-        # Driver code
-
-
-val = [1, 1, 1, 1, 1]
-wt = [10, 20, 30]
-W = 50
-n = len(wt)
-
-printknapSack(W, wt, wt, n)
-
-# This code is contributed by Aryan Garg.
+      print("Packed items: ", packed_items)
+      print("Packed weights: ", packed_weights)
+      print("Total weight (same as total value): ", computed_value)
+if __name__ == '__main__':
+      main()
