79660132b8139116f3192e147014ba400aabe99b
Petre Leonard Macamete <petreleonardos@gmail.com>
2020-02-08 03:36:09 +0200

    one subset sum function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 
 # To find subarray with maximum sum
 # less than or equal to sum
+import time
+
 
 def findMaxSubarraySum(arr, n, sum):
     # To store current sum and
@@ -44,18 +46,33 @@ def subset_sum(numbers, target, partial=[]):
 
     # check if the partial sum is equals to target
     if s == target:
-        found = True
         print("sum(%s)=%s" % (partial, target))
+        raise Exception('element found')
     if s >= target:
         return  # if we reach the number why bother to continue
 
-    if found:
-        return
-
     for i in range(len(numbers)):
         n = numbers[i]
-        remaining = numbers[i+1:]
+        remaining = numbers[i + 1:]
         subset_sum(remaining, target, partial + [n])
+
+
+def one_subset_sum(sum_val: int, set_ints: list):
+    test_subset = []
+    filtered_ints = list(filter(lambda x: x <= sum_val, set_ints))
+    while sum(test_subset) < sum_val and len(set_ints) > 0:
+        remainder = sum_val - sum(test_subset)
+        filtered_ints = list(filter(lambda x: x <= remainder, filtered_ints))
+        if len(filtered_ints) == 0:
+            set_ints.remove(max(set_ints))
+            test_subset = []
+            filtered_ints = list(filter(lambda x: x <= sum_val, set_ints))
+            if sum(set_ints) < sum_val:
+                return []
+            continue
+        test_subset.append(max(filtered_ints))
+        filtered_ints.remove(test_subset[-1])
+    return test_subset
 
 
 # This code is contributed by
@@ -64,13 +81,19 @@ def subset_sum(numbers, target, partial=[]):
 
 def main():
     # Create the solver.
-    file_input = open("c_medium.in", "r")
+    file_input = open("e_also_big.in", "r")
     lines = file_input.read().splitlines()
     setup_values = list(map(int, lines[0].split()))
     pizzas = list(map(int, lines[1].split()))
     capacities = setup_values[0]
     print(findMaxSubarraySum(pizzas, setup_values[1], capacities))
-    subset_sum(pizzas, findMaxSubarraySum(pizzas, setup_values[1], capacities))
+    now = time.time()
+
+    print(sum(one_subset_sum(findMaxSubarraySum(pizzas, setup_values[1], capacities), pizzas)))
+
+    later = time.time()
+    difference = int(later - now)
+    print("Time: "+str(difference))
 
 
 if __name__ == '__main__':
